be8a93fd48714f4f5aac5af9164e919bab93bfb5
aikanba <40813220+aikanba@users.noreply.github.com>
2018-07-27 11:32:57 +0800

    v0.0.1

---
+++ tv/index_files/frameplayer.js
@@ -0,0 +1,7 @@
+function frameplay(src){
+   
+   if (src==''||src==null||src=='undefined'){
+	 document.write('Œ¥…Ë÷√≤•∑≈‘¥!');   return;
+   }
+document.writeln("<iframe src=\"" + src + "\" width=\"100%\" height=\"100%\" marginwidth=\"0\" marginheight=\"0\" hspace=\"0\" vspace=\"0\" frameborder=\"0\" scrolling=\"no\"><\/iframe>");
+}
